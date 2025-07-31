998896beda6feb1aa9fc1f1c2114c1530ce86325
avnitiwari2005 <122551735+avnitiwari2005@users.noreply.github.com>
2025-07-31 14:21:41 +0530

    Add files via upload

---
+++ question3.py
@@ -0,0 +1,9 @@
+n = 2
+if n==1:
+    print("One")
+elif n==2:
+    print("Two")
+elif n==3:
+    print("Three")
+else:
+    print("Other")
---
+++ question4.py
@@ -0,0 +1,6 @@
+x = 5
+y = 10
+if not x > 0 & y < 15:
+    print(x + y)
+else:
+    print(x * y)
